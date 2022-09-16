4b5d7a618ae7933d9d4f81c997803627458b259b
Harry Dent <harry.j.e.dent@gmail.com>
2022-09-16 20:59:34 +0100

    add deploy script

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,9 +1,10 @@
 import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
+  pathPrefix: "/hungry-gods",
   siteMetadata: {
     title: "Hungry Gods",
-    siteUrl: "https://www.yourdomain.tld"
+    siteUrl: "https://hellproxy.github.io/hungry-gods"
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
